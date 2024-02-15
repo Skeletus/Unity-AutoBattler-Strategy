d1ff624ab01fdb2b3373b2c523262f9aef95b3a8
Skeletus <Samol_88@hotmail.com>
2024-02-15 14:16:27 -0500

    adding animator parameters

--- AutoBattlerGame/Assets/Scripts/Unit.cs
+++ AutoBattlerGame/Assets/Scripts/Unit.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
+
     private Vector3 targetPosition;
 
     // Start is called before the first frame update
@@ -21,6 +23,11 @@ public class Unit : MonoBehaviour
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("IsWalking", false);
         }
 
         if (Input.GetMouseButtonDown(0))
