e89abd46c5b156f396fb2a53b50a92843ec00bcd
Skeletus <Samol_88@hotmail.com>
2024-02-16 23:22:33 -0500

    adding visual for selected units using singleton and events

--- AutoBattlerGame/Assets/Scripts/UnitActionSystem.cs
+++ AutoBattlerGame/Assets/Scripts/UnitActionSystem.cs
@@ -1,19 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged;
+
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
 
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one UnitActionSystem! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+    }
 
     private void Update()
     {
-        if (TryHandleUnitSelection()) return;
-
         if (Input.GetMouseButtonDown(0))
         {
+            if (TryHandleUnitSelection()) return;
+
             selectedUnit.Move(MouseWorld.GetPosition());
         }
     }
@@ -25,12 +42,25 @@ public class UnitActionSystem : MonoBehaviour
         {
             if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
 
         return false;
     }
+
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
+    }
+
 
 }
