8751693d51ac56d863518e9a3a356f715407433f
Skeletus <Samol_88@hotmail.com>
2024-02-19 18:09:13 -0500

    fixing error: made spin action child of base action

--- AutoBattlerGame/Assets/Scripts/Actions/SpinAction.cs
+++ AutoBattlerGame/Assets/Scripts/Actions/SpinAction.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpinAction : MonoBehaviour
+public class SpinAction : BaseAction
 {
     private float totalSpinAmount;
 
