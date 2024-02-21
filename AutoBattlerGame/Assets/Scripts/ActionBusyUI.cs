3d2652e6270eae99be55882b85fa4ed5a3fc1b43
Skeletus <Samol_88@hotmail.com>
2024-02-21 01:42:48 -0500

    adding busy bar in buttons UI

---
+++ AutoBattlerGame/Assets/Scripts/ActionBusyUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionBusyUI : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnitActionSystem.Instance.OnBusyChanged += UnitActionSystem_OnBusyChanged;
+
+        Hide();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void UnitActionSystem_OnBusyChanged(object sender, bool isBusy)
+    {
+        if (isBusy)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+}
--- AutoBattlerGame/Assets/Scripts/UnitActionSystem.cs
+++ AutoBattlerGame/Assets/Scripts/UnitActionSystem.cs
@@ -12,6 +12,7 @@ public class UnitActionSystem : MonoBehaviour
 
     public event EventHandler OnSelectedUnitChanged;
     public event EventHandler OnSelectedActionChanged;
+    public event EventHandler<bool> OnBusyChanged;
 
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
@@ -59,11 +60,13 @@ public class UnitActionSystem : MonoBehaviour
     private void SetBusy()
     {
         isBusy = true;
+        OnBusyChanged?.Invoke(this, isBusy);
     }
 
     private void ClearBusy()
     {
         isBusy = false;
+        OnBusyChanged?.Invoke(this, isBusy);
     }
 
     private bool TryHandleUnitSelection()
