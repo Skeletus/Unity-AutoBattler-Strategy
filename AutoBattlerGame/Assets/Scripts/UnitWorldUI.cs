8ae4dac317f9221532a420a9bc30721f4d877897
Skeletus <Samol_88@hotmail.com>
2024-02-25 15:21:11 -0500

    adding health bar UI on units

--- AutoBattlerGame/Assets/Scripts/UnitWorldUI.cs
+++ AutoBattlerGame/Assets/Scripts/UnitWorldUI.cs
@@ -3,16 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitWorldUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI actionPointsText;
     [SerializeField] private Unit unit;
+    [SerializeField] private Image healthBarImage;
+    [SerializeField] private HealthSystem healthSystem;
 
     private void Start()
     {
         Unit.OnAnyActionPointsChanged += Unit_OnAnyActionPointsChanged;
+        healthSystem.OnDamaged += HealthSystem_OnDamaged;
+
         UpdateActionPointsText();
+        UpdateHealthBar();
+    }
+
+    private void HealthSystem_OnDamaged(object sender, EventArgs e)
+    {
+        UpdateHealthBar();
     }
 
     private void Unit_OnAnyActionPointsChanged(object sender, EventArgs e)
@@ -23,5 +34,10 @@ public class UnitWorldUI : MonoBehaviour
     private void UpdateActionPointsText()
     {
         actionPointsText.text = unit.GetActionPoints().ToString();
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBarImage.fillAmount = healthSystem.GetHealthNormalized();
     }
 }
