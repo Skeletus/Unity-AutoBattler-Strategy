58203e4c3047a2a9b7881f609295f67c3d0033ce
Skeletus <Samol_88@hotmail.com>
2024-02-28 21:28:15 -0500

    adding basic states for enemy AI

--- AutoBattlerGame/Assets/Scripts/EnemyAI.cs
+++ AutoBattlerGame/Assets/Scripts/EnemyAI.cs
@@ -5,7 +5,20 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    private enum State
+    {
+        WaitingForEnemyTurn,
+        TakingTurn,
+        Busy,
+    }
+
+    private State state;
     private float timer;
+
+    private void Awake()
+    {
+        state = State.WaitingForEnemyTurn;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +34,31 @@ public class EnemyAI : MonoBehaviour
             return;
         }
 
-        timer -= Time.deltaTime;
-        if (timer <= 0f)
+        switch(state)
         {
-            TurnSystem.Instance.NextTurn();
+            case State.WaitingForEnemyTurn:
+                break;
+            case State.TakingTurn:
+                timer -= Time.deltaTime;
+                if (timer <= 0f)
+                {
+                    state = State.WaitingForEnemyTurn;
+                    TurnSystem.Instance.NextTurn();
+                }
+                break;
+            case State.Busy:
+                break;
         }
+
     }
 
     private void TurnSystem_OnTurnChanged(object sender, EventArgs e)
     {
-        timer = 2f;
+        if (!TurnSystem.Instance.IsPlayerTurn())
+        {
+            state = State.TakingTurn;
+            timer = 2f;
+        }
     }
+
 }
