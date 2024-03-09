92c10e099e996e183edd6daf60c3598fc34a605d
Skeletus <Samol_88@hotmail.com>
2024-03-08 23:12:53 -0500

    creating screen shake script using CM impulse ext

---
+++ AutoBattlerGame/Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ScreenShake : MonoBehaviour
+{
+    private CinemachineImpulseSource cinemachineImpulseSource;
+
+    private void Awake()
+    {
+        cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            cinemachineImpulseSource.GenerateImpulse();
+        }
+    }
+}
