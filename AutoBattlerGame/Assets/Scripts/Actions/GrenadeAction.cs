f958dbf231670f207495dcb72c75b84ab320b00c
Skeletus <Samol_88@hotmail.com>
2024-03-09 14:50:31 -0500

    updating grenade script: added circular valid throwable grid

--- AutoBattlerGame/Assets/Scripts/Actions/GrenadeAction.cs
+++ AutoBattlerGame/Assets/Scripts/Actions/GrenadeAction.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class GrenadeAction : BaseAction
 {
+    private int maxThrowDistance = 7;
+
     private void Update()
     {
         if (!isActive)
@@ -31,12 +33,33 @@ public class GrenadeAction : BaseAction
 
     public override List<GridPosition> GetValidActionGridPositionList()
     {
+        List<GridPosition> validGridPositionList = new List<GridPosition>();
+
         GridPosition unitGridPosition = unit.GetGridPosition();
 
-        return new List<GridPosition> 
-        { 
-            unitGridPosition
-        };
+        for (int x = -maxThrowDistance; x <= maxThrowDistance; x++)
+        {
+            for (int z = -maxThrowDistance; z <= maxThrowDistance; z++)
+            {
+                GridPosition offsetGridPosition = new GridPosition(x, z);
+                GridPosition testGridPosition = unitGridPosition + offsetGridPosition;
+
+                if (!LevelGrid.Instance.IsValidGridPosition(testGridPosition))
+                {
+                    continue;
+                }
+
+                int testDistance = Mathf.Abs(x) + Mathf.Abs(z);
+                if (testDistance > maxThrowDistance)
+                {
+                    continue;
+                }
+
+                validGridPositionList.Add(testGridPosition);
+            }
+        }
+
+        return validGridPositionList;
     }
 
     public override void TakeAction(GridPosition gridPosition, Action onActionComplete)
