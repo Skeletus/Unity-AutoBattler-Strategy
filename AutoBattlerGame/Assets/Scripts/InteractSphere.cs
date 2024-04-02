5bd39031fdd5de65f02c3a6a43cd5fca72d70b3c
Skeletus <Samol_88@hotmail.com>
2024-04-01 22:34:04 -0500

    added interactable sphere object

---
+++ AutoBattlerGame/Assets/Scripts/InteractSphere.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractSphere : MonoBehaviour
+{
+    [SerializeField] private Material greenMaterial;
+    [SerializeField] private Material redMaterial;
+    [SerializeField] private MeshRenderer meshRenderer;
+
+    private bool isGreen;
+
+    private void Start()
+    {
+        SetColorGreen();
+    }
+
+    private void SetColorGreen()
+    {
+        isGreen = true;
+        meshRenderer.material = greenMaterial;
+    }
+
+    private void SetColorRed()
+    {
+        isGreen = false;
+        meshRenderer.material = redMaterial;
+    }
+}
